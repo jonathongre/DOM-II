e70cff5610fa3c2c4c5dd477e3e14843b6948d07
Jonathon Green <ericg9811@gmail.com>
2019-07-09 15:32:12 -0500

    Added event listener to nav links to change their color on mouseover and return to original color on mouseout. Images rotate on wheel. The body of the page has an event listener that changes the background color on dblclick. All paragraphs change font color and background color on focus and blur. H2 and H4 tags are made bold on click.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
 // Your code goes here
+
+const navLink = document.querySelectorAll('.nav-link')
+const home = document.querySelector('.home')
+const titles = document.querySelectorAll('h2, h4')
+const paragraphs = document.querySelectorAll('p')
+const images = document.querySelectorAll('img')
+const footer = document.querySelector('.footer p')
+const button = document.querySelectorAll('.btn')
+
+images.forEach(element => {
+    element.addEventListener('wheel', event => {
+        event.preventDefault();
+        event.target.style.transform = 'rotate(0.5turn)';
+    });
+})
+
+
+navLink.forEach(element => {
+    element.addEventListener('mouseover', event => {
+        event.target.style.color = '#008800'
+    })
+
+    element.addEventListener('mouseout', event => {
+        event.target.style.color = '#000'
+    })
+})
+
+home.addEventListener('dblclick', event => {
+    home.style.backgroundColor = '#6a6a6a';
+})
+
+titles.forEach(element => {
+    element.addEventListener('click', event => {
+        event.target.style.fontWeight = 'bold';
+    })
+})
+
+paragraphs.forEach(element => {
+    element.tabIndex = 1
+    element.addEventListener('focus', event => {
+        event.target.style.backgroundColor = '#000'
+        event.target.style.color = '#fff'
+    });
+    element.addEventListener('blur', event => {
+        event.target.style.background = ''
+        event.target.style.color = ''
+    });
+})
